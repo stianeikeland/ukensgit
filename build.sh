b3d090664be6e1793d0e08ce75249ca03381abce
Stian Eikeland <stian@eikeland.se>
2013-11-01 15:08:30 +0100

    templates

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "<html><body>" > index.html
+
+for f in *.md; do
+	BASE=`basename ${f} .md`
+	cat templatetop.html > ${BASE}.html
+	multimarkdown $f | sed 's/<pre><code>/<pre><code class="prettyprint lang-sh">/g'>> ${BASE}.html
+	cat templatebottom.html >> ${BASE}.html
+
+	echo "<a href='${BASE}.html'>${BASE}</a><br />" >> index.html
+done
+
+echo "</body></html>" >> index.html
+
+cp *.html /Users/se/Dropbox/Public/ukensgit/
